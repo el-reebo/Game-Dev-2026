3f87a8eb3457ff8e0a3e2166ccebd4129c2251a6
ReezoPeez0 <118624146+ReezoPeez0@users.noreply.github.com>
2026-03-27 18:17:46 +0000

    Implemented Call and Response Feature
    When calling the name "Charlie" the monster will now respond with an audio clip
    
    Currently using placeholder sound until appropriate sound effect found

--- Assets/Scripts/VoiceDetection.cs
+++ Assets/Scripts/VoiceDetection.cs
@@ -7,13 +7,21 @@ using UnityEngine.Windows.Speech;
 
 public class VoiceDetection : MonoBehaviour
 {
+    [Header("Monster SFX")]
+    [SerializeField] private AudioClip monsterRoar;
+    [SerializeField] private float voiceCallDelay = 1.0f;
+    
     private Animator m_Animator;
-
+    private AudioSource monsterAudio;
     private KeywordRecognizer keywordRecognizer;
     private Dictionary<string, Action> monsterActions = new Dictionary<string, Action>();
 
+    private float lastVoiceCall;
+
     void Start()
     {
+        monsterAudio = GetComponent<AudioSource>();
+
         // Check mic detection
         foreach (var device in Microphone.devices)
         {
@@ -22,8 +30,11 @@ public class VoiceDetection : MonoBehaviour
 
         m_Animator = GetComponent<Animator>();
 
+        // Recognised words
         monsterActions.Add("Stay", Stay);
+        monsterActions.Add("Charlie", Charlie);
 
+        // Initialise keyword recogniser
         keywordRecognizer = new KeywordRecognizer(monsterActions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += RecognizedPhrase;
         keywordRecognizer.Start();
@@ -37,8 +48,19 @@ public class VoiceDetection : MonoBehaviour
         monsterActions[phrase.text].Invoke();
     }
 
+    // --- Voice activation functions ---
     void Stay()
     {
         Debug.Log("Stay function played");
+    }
+
+    void Charlie()
+    {
+        if (Time.time > lastVoiceCall + voiceCallDelay)
+        {
+            monsterAudio.PlayOneShot(monsterRoar, 1.0f);
+            lastVoiceCall = Time.time;
+            Debug.Log("Charlie function played");
+        }
     }
 }
